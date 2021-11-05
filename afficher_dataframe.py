5471b2b424ef7a725ff2ed7c9bb5ea1180dcf08c
Émile Jetzer <emile.jetzer@polymtl.ca>
2021-11-05 14:15:47 -0400

    Afficahge et modification de DataFrames

--- afficher_dataframe.py
+++ afficher_dataframe.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+Programme pour afficher et modifier visuellement des DataFrames Pandas.
+
 Created on Tue Nov  2 15:40:02 2021
 
 @author: ejetzer
@@ -13,10 +15,36 @@ import pandas as pd
 
 class Tableau(tk.Frame):
 
-    def __init__(self, master=None, données: pd.DataFrame = None, *args, **kargs):
-        self.données = données.copy()
+    def __init__(self, master, tableau: pd.DataFrame, *args, **kargs):
+        self.tableau = tableau.copy()
 
         super().__init__(master, *args, **kargs)
+        self.__init_tableau(self.tableau)
+
+    def __init_rangée_titres(self, tableau: pd.DataFrame):
+        return [tk.Label(self, text=col) for col in tableau.columns]
+
+    def __init_colonne_index(self, tableau: pd.DataFrame):
+        return [tk.Label(self, text=k) for k, rang in tableau.iterrows()]
+
+    def __init_tableau_contenu(self, tableau: pd.DataFrame):
+        entrées = []
+
+        for k, rang in tableau.iterrows():
+            entrées.append([])
+
+            for col in rang:
+                variable = tk.StringVar(self, col)
+                variable.trace_add('write', lambda x, i, m, v=variable: self.update_tableau())
+                entrée = tk.Entry(self, textvariable=variable)
+                entrées[-1].append(entrée)
+
+        return entrées
+
+    def __init_tableau(self, tableau):
+        self.rangée_titres = self.__init_rangée_titres(tableau)
+        self.colonne_index = self.__init_colonne_index(tableau)
+        self.tableau_contenu = self.__init_tableau_contenu(tableau)
 
     def grid(self,
              row: int,
@@ -28,21 +56,34 @@ class Tableau(tk.Frame):
              firstcolumn: int = 0,
              **kargs):
 
-        défaut_span = self.données.shape
+        défaut_span = self.tableau.shape
 
-        if rowspan is None:
-            rowspan = défaut_span[0] + 1
-        elif columnspan is None:
-            columnspan = défaut_span[1] + 1
+        if rowspan is None: rowspan = défaut_span[0] + 1
+        if columnspan is None: columnspan = défaut_span[1] + 1
 
-        tableau = self.données.iloc[firstrow:firstrow+rowspan, firstcolumn:firstcolumn+columnspan]
+        cols = slice(firstcolumn, firstcolumn+columnspan)
+        rangs = slice(firstrow, firstrow+rowspan)
 
-        colonnes = tableau.columns
+        for i, col in enumerate(self.rangée_titres[cols]):
+            col.grid(row=0, column=i+1)
 
-        for i, col in enumerate(colonnes):
-            pass
+        for i, (idx, rang) in enumerate(zip(self.colonne_index[rangs], self.tableau_contenu[rangs])):
+            idx.grid(row=i+1, column=0)
 
-        for i, rang in enumerate(tableau.iterrows()):
-            pass
+            for j, col in enumerate(rang[cols]):
+                col.grid(row=i+1, column=j+1)
 
         super().grid(row=row, column=column, columnspan=columnspan, rowspan=rowspan)
+
+    def update_tableau(self, *args, **kargs):
+        for idx, rang in zip(self.colonne_index, self.tableau_contenu):
+            for col, val in zip(self.rangée_titres, rang):
+                print(val)
+                self.tableau.loc[idx['text'], col['text']] = val.get()
+
+if __name__ == '__main__':
+    racine = tk.Tk()
+    cadre = pd.DataFrame({'a': [1, 2, 3], 'b': [1, 3, 2]})
+    tableau = Tableau(racine, cadre)
+    tableau.grid(0, 0)
+    racine.mainloop()
