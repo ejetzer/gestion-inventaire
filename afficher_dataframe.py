41a500d036e4e5221495fdf85763c077ddcebd92
Émile Jetzer <emile.jetzer@polymtl.ca>
2021-11-09 14:21:38 -0500

    Ajouter des rangées, et faire défiler les grands tableaux.

--- afficher_dataframe.py
+++ afficher_dataframe.py
@@ -8,53 +8,59 @@ Created on Tue Nov  2 15:40:02 2021
 @author: ejetzer
 """
 
+import itertools as it
 import tkinter as tk
+
 import sqlalchemy as db
 import pandas as pd
 
 
-class Tableau(tk.Frame):
+class Tableau:
+    """Wrapper for pandas.DataFrame & tkinter.Frame."""
 
-    def __init__(self, master, tableau: pd.DataFrame, *args, **kargs):
+    def __init__(self, master, tableau: pd.DataFrame):
+        """Wrap DataFrame & Frame."""
         self.tableau = tableau.copy()
+        self.master = master
 
-        super().__init__(master, *args, **kargs)
-        self.__init_tableau(self.tableau)
+        self.__init_tableau()
 
-    def __init_rangée_titres(self, tableau: pd.DataFrame):
-        return [tk.Label(self, text=col) for col in tableau.columns]
+    def __init_rangée_titres(self):
+        return [tk.Label(self.master, text=col) for col in self.tableau.columns]
 
-    def __init_colonne_index(self, tableau: pd.DataFrame):
-        return [tk.Label(self, text=k) for k, rang in tableau.iterrows()]
+    def __init_colonne_index(self):
+        return [tk.Label(self.master, text=k) for k, rang in self.tableau.iterrows()]
 
-    def __init_tableau_contenu(self, tableau: pd.DataFrame):
+    def __init_tableau_contenu(self):
         entrées = []
 
-        for k, rang in tableau.iterrows():
+        for k, rang in self.tableau.iterrows():
             entrées.append([])
 
             for col in rang:
-                variable = tk.StringVar(self, col)
+                variable = tk.StringVar(self.master, col)
                 variable.trace_add('write', lambda x, i, m, v=variable: self.update_tableau())
-                entrée = tk.Entry(self, textvariable=variable)
+                entrée = tk.Entry(self.master, textvariable=variable)
                 entrées[-1].append(entrée)
 
         return entrées
 
-    def __init_tableau(self, tableau):
-        self.rangée_titres = self.__init_rangée_titres(tableau)
-        self.colonne_index = self.__init_colonne_index(tableau)
-        self.tableau_contenu = self.__init_tableau_contenu(tableau)
+    def __init_tableau(self):
+        self.rangée_titres = self.__init_rangée_titres()
+        self.colonne_index = self.__init_colonne_index()
+        self.tableau_contenu = self.__init_tableau_contenu()
 
     def grid(self,
              row: int,
              column: int,
              rowspan: int = None,
              columnspan: int = None,
-             *args,
              firstrow: int = 0,
-             firstcolumn: int = 0,
-             **kargs):
+             firstcolumn: int = 0):
+        """Display the DataFrame."""
+        self.__grid_params = {'row': row, 'column': column,
+                              'rowspan': rowspan, 'columnspan': columnspan,
+                              'firstrow': firstrow, 'firstcolumn': firstcolumn}
 
         défaut_span = self.tableau.shape
 
@@ -65,26 +71,57 @@ class Tableau(tk.Frame):
         rangs = slice(firstrow, firstrow+rowspan)
 
         for i, col in enumerate(self.rangée_titres[cols]):
-            col.grid(row=0, column=i+1)
+            col.grid(row=row, column=column+i+1)
 
         for i, (idx, rang) in enumerate(zip(self.colonne_index[rangs], self.tableau_contenu[rangs])):
-            idx.grid(row=i+1, column=0)
+            idx.grid(row=row+i+1, column=column)
 
             for j, col in enumerate(rang[cols]):
-                col.grid(row=i+1, column=j+1)
+                col.grid(row=row+i+1, column=column+j+1)
 
-        super().grid(row=row, column=column, columnspan=columnspan, rowspan=rowspan)
 
     def update_tableau(self, *args, **kargs):
         for idx, rang in zip(self.colonne_index, self.tableau_contenu):
             for col, val in zip(self.rangée_titres, rang):
                 self.tableau.loc[idx['text'], col['text']] = val.get()
 
+    def update_grid(self):
+        'Update the grid after a change to the DataFrame'
+        for widget in it.chain(self.rangée_titres, self.colonne_index, *self.tableau_contenu):
+            widget.destroy()
+
+        self.__init_tableau()
+        self.grid(**self.__grid_params)
+
+    def ajouter_rangée(self):
+        self.tableau = self.tableau.append(pd.Series(), ignore_index=True)
+        self.update_grid()
+
 if __name__ == '__main__':
     racine = tk.Tk()
+    racine.title('Base de données')
+
+    canevas = tk.Canvas(racine, width='50c', height='15c')
+    défiler_horizontalement = tk.Scrollbar(racine, orient='horizontal', command=canevas.xview)
+    défiler_verticalement = tk.Scrollbar(racine, orient='vertical', command=canevas.yview)
+    canevas.configure(xscrollcommand=défiler_horizontalement.set,
+                      yscrollcommand=défiler_verticalement.set)
+    contenant = tk.Frame(canevas)
+    contenant.bind('<Configure>', lambda x: canevas.configure(scrollregion=canevas.bbox('all')))
+
     cadre = pd.read_sql_table('test', 'sqlite:///référence.db', index_col='index')
-    tableau = Tableau(racine, cadre)
+    tableau = Tableau(contenant, cadre)
+
+    màj = tk.Button(racine, text='Màj', command=lambda: tableau.update_grid())
+    rangée = tk.Button(racine, text='+', command=lambda: tableau.ajouter_rangée())
+
+    défiler_horizontalement.grid(row=16, column=1, columnspan=1, sticky='we')
+    défiler_verticalement.grid(row=1, column=2, rowspan=15, sticky='ns')
+    canevas.grid(row=1, column=1, rowspan=15, sticky='news')
+    canevas.create_window((30, 15), window=contenant)
     tableau.grid(0, 0)
+    màj.grid(row=0, column=0)
+    rangée.grid(row=1, column=0)
     racine.mainloop()
 
     engine = db.create_engine('sqlite:///référence.db')
