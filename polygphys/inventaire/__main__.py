8acdd2e3e0a9c2752cf92a00bbe5805c528a6e73
Émile Jetzer <emile.jetzer@polymtl.ca>
2022-03-03 11:47:19 -0500

    Ajout du programme d'inventaire externe

--- polygphys/inventaire/__main__.py
+++ polygphys/inventaire/__main__.py
@@ -8,7 +8,51 @@ Créé le Thu Dec 16 13:37:19 2021
 @author: ejetzer
 """
 
+import platform
+
+from pathlib import Path
+
 from . import main
 
+CHEMINS = {'Darwin': Path('/Volumes/GeniePhysique/Techniciens/'),
+           'Windows': Path(r'Z:'),
+           None: Path('~/.inventaire').expanduser().resolve()}
+SOUS_CHEMIN = Path('Emile_Jetzer/Inventaire/')
+
 if __name__ == '__main__':
+    dossier = CHEMINS.get(platform.system(), CHEMINS[None]) / SOUS_CHEMIN
+
+    if not dossier.exists():
+        dossier.mkdir()
+        cfg = dossier / 'default.cfg'
+        db = dossier / 'inventaire.sqlite'
+
+        with cfg.open('w') as f:
+            f.write(f'''[bd]
+    adresse = sqlite:///{db}
+tables =
+    personnes
+	locaux
+	portes
+	etageres
+	appareils
+	boites
+	emprunts
+	utilisation_boites
+formulaires = 
+	personnes
+	locaux
+	portes
+	etageres
+	appareils
+	boites
+	emprunts
+	utilisation_boites
+
+[tkinter]
+title: Inventaire du département de génie physique à Polytechnique Montréal
+''')
+
+        db.touch()
+        
     main()
