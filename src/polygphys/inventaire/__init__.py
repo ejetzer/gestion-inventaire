a17bee607425a847fb2b411338a5608919cc6cd2
Émile Jetzer <emile.jetzer@polymtl.ca>
2022-03-25 10:04:39 -0400

    Correction d'un problème avec le programme d'inventaire, maintenant le montage se fait dans le bon sous-dossier, donc le loquet fonctionne.

--- src/polygphys/inventaire/__init__.py
+++ src/polygphys/inventaire/__init__.py
@@ -8,11 +8,9 @@ Créé le Fri Nov 26 15:15:36 2021
 @author: ejetzer
 """
 
-import logging
-import sys
-import platform
-
 import tkinter as tk
+import keyring
+import getpass
 
 from pathlib import Path
 
@@ -21,138 +19,50 @@ from sqlalchemy import MetaData
 from ..outils.config import FichierConfig
 from ..outils.database import BaseDeDonnées
 from ..outils.interface.tkinter.onglets import Onglets
-from ..outils.journal import Formats
 from ..outils.reseau import DisqueRéseau
-
 from ..inventaire.modeles import créer_dbs
 
 
-def main(dossier=None):
-    """Programme de gestion d'inventaire."""
-    logging.debug('dossier = %r', dossier)
+class InventaireConfig(FichierConfig):
 
-    logging.info('Chargement de la configuration...')
-
-    if dossier is None:
-        if len(sys.argv) > 1:
-            dossier = Path(sys.argv[1]).resolve()
-        else:
-            fichier = Path(__file__).expanduser().resolve()
-            dossier = fichier.parent
-
-    cfg = dossier / next(x.name for x in dossier.glob('*.cfg'))
-    logging.debug('cfg = %r', cfg)
-
-    config = FichierConfig(cfg)
-    logging.debug('config = %r', config)
-
-    for sec in config.sections():
-        logging.info('[%r]', sec)
-        for c, v in config[sec].items():
-            logging.info('%r: %r', c, v)
-
-    logging.info('Chargement de la base de données...')
-
-    adresse = config.geturl('bd', 'adresse')
-    logging.debug('adresse = %r', adresse)
-
-    metadata = créer_dbs(MetaData())
-    base = BaseDeDonnées(adresse, metadata)
-    logging.debug('base = %r', base)
-
-    base.initialiser()
-
-    for n, t in base.tables.items():
-        logging.info('[%r]', n)
-        for c in t.columns:
-            logging.info('%r', c)
-
-    logging.info(base.select('boites'))
-    logging.info(base.select('appareils'))
-
-    logging.info('Préparation de l\'interface...')
-    racine = tk.Tk()
-    racine.title(config.get('tkinter', 'title', fallback='Inventaire'))
-
-    logging.info('Chargement de la base de données...')
-    onglets = Onglets(racine, config, metadata)
-
-    onglets.grid(sticky='nsew')
-    racine.mainloop()
+    def default(self):
+        return (Path(__file__).parent / 'default.cfg').open().read()
 
 
-def script():
-    """
-    Script pour inventaire partagé.
+def main():
+    fichier_config = Path('~/inventaire.cfg').expanduser()
+    config_locale = InventaireConfig(fichier_config)
 
-    Returns
-    -------
-    None.
+    chemin = config_locale.get('disque', 'chemin')
+    chemin = str(Path(chemin).expanduser())
+    config_locale.set('disque', 'chemin', chemin)
 
-    """
-    # Informations du disque réseau
-    config_réseau = FichierConfig(Path('~/heures.cfg').expanduser())
-    chemin = config_réseau.getpath('Volumes', 'chemin')
-    adresse = config_réseau.get('Volumes', 'adresse')
-    nom = config_réseau.get('Volumes', 'nom')
-    mdp = config_réseau.get('Volumes', 'mdp')
+    adresse = config_locale.get('disque', 'adresse')
+    nom = config_locale.get('disque', 'nom')
+    mdp = keyring.get_password('system',
+                               mdp_id := f'polygphys.inventaire.main.disque.{adresse}.{nom}')
 
-    disque = DisqueRéseau(adresse, chemin, 'Z:', nom, mdp)
+    if mdp is None:
+        mdp = getpass.getpass('mdp>')
+        keyring.set_password('system', mdp_id, mdp)
 
-    SOUS_CHEMIN = Path('Techniciens/Emile_Jetzer/Inventaire/')
-
-    with disque:
-
-        dossier = disque / SOUS_CHEMIN
-
-        if not dossier.exists():
-            dossier.mkdir()
-            cfg = dossier / 'default.cfg'
-            db = dossier / 'inventaire.sqlite'
-
-            with cfg.open('w') as f:
-                with (Path(__file__).parent / 'default.cfg').open('r') as g:
-                    f.write(g.read().format(db=db))
-
-            db.touch()
-
-        cfg = dossier / next(x.name for x in dossier.glob('*.cfg'))
-        logging.debug('cfg = %r', cfg)
-
-        config = FichierConfig(cfg)
-        logging.debug('config = %r', config)
-
-        for sec in config.sections():
-            logging.info('[%r]', sec)
-            for c, v in config[sec].items():
-                logging.info('%r: %r', c, v)
-
-        logging.info('Chargement de la base de données...')
-
-        adresse = f"sqlite:////{dossier / config.get('bd', 'nom')}"
-        config.set('bd', 'adresse', adresse)
-        logging.debug('adresse = %r', adresse)
+    with DisqueRéseau(**config_locale['disque'], mdp=mdp) as disque:
+        config_disque = InventaireConfig(disque / 'inventaire.cfg')
 
         metadata = créer_dbs(MetaData())
-        base = BaseDeDonnées(adresse, metadata)
-        logging.debug('base = %r', base)
+        nom = config_disque.get('bd', 'nom',
+                                fallback=config_locale.get('bd',
+                                                           'nom'))
+        adresse = f'sqlite:///{disque / nom}'
+        config_disque.set('bd', 'adresse', adresse)
+        base_de_données = BaseDeDonnées(adresse, metadata)
+        base_de_données.initialiser()
 
-        base.initialiser()
-
-        for n, t in base.tables.items():
-            logging.info('[%r]', n)
-            for c in t.columns:
-                logging.info('%r', c)
-
-        logging.info(base.select('boites'))
-        logging.info(base.select('appareils'))
-
-        logging.info('Préparation de l\'interface...')
         racine = tk.Tk()
-        racine.title(config.get('tkinter', 'title', fallback='Inventaire'))
-
-        logging.info('Chargement de la base de données...')
-        onglets = Onglets(racine, config, metadata)
-
+        titre = config_disque.get('tkinter', 'titre',
+                                  fallback=config_locale.get('tkinter',
+                                                             'titre'))
+        racine.title(titre)
+        onglets = Onglets(racine, config_disque, metadata)
         onglets.grid(sticky='nsew')
         racine.mainloop()
--- src/polygphys/inventaire/__main__.py
+++ src/polygphys/inventaire/__main__.py
@@ -10,10 +10,10 @@ Créé le Thu Dec 16 13:37:19 2021
 
 import logging
 
-from . import script
+from . import main
 
 from polygphys.outils.journal import Formats
 
 if __name__ == '__main__':
     logging.basicConfig(format=Formats().détails, level=logging.DEBUG)
-    script()
+    main()
